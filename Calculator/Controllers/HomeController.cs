78f0298209c0aebd39ebd2169d6451fb0ae906e8
Victor Thuillier <victor.thuillier@gmail.com>
2015-01-22 15:00:11 +0100

    first commit

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            double nbr1 = Convert.ToDouble(Request.QueryString.Get("nbr1"));
+            double nbr2 = Convert.ToDouble(Request.QueryString.Get("nbr2"));
+
+            string op = Request.QueryString.Get("op");
+
+            switch (op)
+            {
+                case "+":
+                    ViewBag.res = nbr1 + nbr2;
+                    break;
+
+                case "-":
+                    ViewBag.res = nbr1 - nbr2;
+                    break;
+
+                case "*":
+                    ViewBag.res = nbr1 * nbr2;
+                    break;
+
+                case "/":
+                    ViewBag.res = nbr1 / nbr2;
+                    break;
+
+                default:
+                    ViewBag.res = "Invalid operator";
+                    break;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("e9287d56-454b-489c-a6bc-592bcb7cca41")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
